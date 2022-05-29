207f2a3876bc15d0850262a78e898d08384c60c3
Daniil Laguteev <daniillaga121@yandex.ru>
2022-05-29 15:34:25 +0300

    two lesson

---
+++ script.js
@@ -0,0 +1,15 @@
+let title = 'lending';
+    console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+    console.log(screens.length);
+    console.log(screens.toLowerCase());
+    console.log(screens.split(''));
+let screenPrice = 24;
+    console.log(screenPrice ,screenPrice * 70 ,screenPrice * 2.23 ,screenPrice * 9.97);
+let rollback = 71;
+let fullPrice = 500;
+    console.log(typeof fullPrice);
+    console.log(fullPrice ,fullPrice * 70 ,fullPrice * 2.23 ,fullPrice * 9.97);
+    console.log(fullPrice * (rollback/100));
+let adaptive = true;
+    console.log(typeof adaptive);
