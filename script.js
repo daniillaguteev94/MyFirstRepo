f727c370b8d1cd3fd45f8c3b707f70eeb2df5656
Daniil Laguteev <daniillaga121@yandex.ru>
2022-05-29 21:57:42 +0300

    rework lessontwo v1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let title = 'lending';
-    console.log(typeof title);
+const title = 'lending';
 let screens = "Простые, Сложные, Интерактивные";
-    console.log(screens.length);
-    console.log(screens.toLowerCase());
-    console.log(screens.split(''));
-let screenPrice = 24;
-    console.log(screenPrice ,screenPrice * 70 ,screenPrice * 2.23 ,screenPrice * 9.97);
-let rollback = 71;
-let fullPrice = 500;
-    console.log(typeof fullPrice);
-    console.log(fullPrice ,fullPrice * 70 ,fullPrice * 2.23 ,fullPrice * 9.97);
-    console.log(fullPrice * (rollback/100));
-let adaptive = true;
-    console.log(typeof adaptive);
+const screenPrice = 24;
+const rollback = 71;
+const fullPrice = 500;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof adaptive);
+console.log(typeof fullPrice);
+
+console.log(screens.length ,screens.toLowerCase() ,screens.split('-' ,3));
+
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рубля ');
+console.log('Стоимость разаработки сайта ' + fullPrice + ' рублей ');
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback/100));
